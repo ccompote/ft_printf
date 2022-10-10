e910960e836bbd81189f32d4e424852172a7b539
ccompote <102297466+ccompote@users.noreply.github.com>
2022-10-10 20:07:20 +0200

    Add files via upload

--- ft_printf2.c
+++ ft_printf2.c
@@ -6,7 +6,7 @@
 /*   By: ccompote <ccompote@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 16:20:57 by ccompote          #+#    #+#             */
-/*   Updated: 2022/10/06 17:45:22 by ccompote         ###   ########.fr       */
+/*   Updated: 2022/10/10 19:41:34 by ccompote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,10 @@ int	ft_putstr(char *s)
 
 	i = 0;
 	if (s == NULL)
-		return (0);
+    {
+        write(1, "(null)", 6);
+        return(6);
+    }
 	while (s[i] != '\0')
 	{
 		ft_putchar(s[i]);
@@ -54,6 +57,55 @@ void	ft_putnbr(int n)
 	}
 }
 
+int ft_point(unsigned long long x)
+{
+/*     if (x == 0)
+    {
+       write(1, "0", 1);
+       return(1);
+    } */
+    if (x >= 16)
+    {
+        hex(x / 16);
+        hex(x % 16);
+    }
+    else
+    {
+        if (x <= 9)
+            ft_putchar(x + '0');
+        else
+            ft_putchar(x - 10 + 'a');
+    }
+    return(0);
+}
+
+int ft_hex(unsigned long long x, int c)
+{
+    if (x >= 16)
+    {
+        hex(x / 16);
+        hex(x % 16);
+    }
+    else
+    {
+        if (x <= 9)
+            ft_putchar(x + '0');
+        else
+            if (x == 'x')
+               ft_putchar(x - 10 + 'a'); 
+            else
+                ft_putchar(x - 10 + 'A');
+    }
+    return(0);
+}
+
+int ft_uint(unsigned int n)
+{
+    if (n >= 10)
+		ft_uint(n / 10);
+	ft_uint(n % 10 + '0');
+}
+
 int ft_check_format(char c)
 {
     if ((c == 'c') || (c == 's') || (c == 'p') || (c == 'd') || (c == 'i')
@@ -68,24 +120,31 @@ int ft_worktype(int c, va_list ap)
 
     count = 0;
     if (c == 'c')
-        ft_putchar(va_arg(ap, int));
+        count += ft_putchar(va_arg(ap, int));
     if (c == 's')
-        ft_putstr(va_arg(ap, char *));
-    //else if (c == 'p')
+        count += ft_putstr(va_arg(ap, char *));
+    else if (c == 'p')
+        count += ft_hex(va_arg(ap, unsigned long));
     else if (c == 'd' || c == 'i')
-        ft_putnbr(va_arg(ap, int));
-    //else if (c == 'u')
-    //else if (c == 'x' || c == 'X')
+        count += ft_putnbr(va_arg(ap, int));
+    else if (c == 'u')
+        count += ft_uint(va_arg(ap, unsigned int));
+    else if (c == 'x' || c == 'X')
+        count += ft_hex(va_arg(ap, unsigned int, int c));
     else if (c == '%')
-        ft_putchar(c);
+        count += ft_putchar(c);
     return(count);
 }
 
-void ft_workstr(const char *s, va_list ap)
+int ft_printf(const char *s, ...)
 {
+    int count;
     int i;
-
+    va_list ap;
+    
+    count = 0;
     i = 0;
+    va_start(ap, s);
     while(s[i])
     {
         if(s[i] == '%' && s[i + 1])
@@ -93,23 +152,13 @@ void ft_workstr(const char *s, va_list ap)
             i++;
             if(ft_check_format(s[i]))
             {
-                ft_worktype(s[i], ap);
+                count += ft_worktype(s[i], ap);
             }
         }
         else if (s[i] != '%')
-            ft_putchar(s[i]);
+            count += ft_putchar(s[i]);
         i++;
     }
-}
-
-int ft_printf(const char *s, ...)
-{
-    int count;
-    va_list ap;
-    
-    count = 0;
-    va_start(ap, s);
-    ft_workstr(s, ap);
     va_end(ap); 
     return(count);
 }
@@ -119,6 +168,6 @@ int main(void)
     char *p;
     ft_printf("Hello %s, you\n", "Anna");
     printf("Hello %s, you\n", "Anna");
-    printf("%p\n", p);
+    printf("%s\n", p);
     return(0);
 }
